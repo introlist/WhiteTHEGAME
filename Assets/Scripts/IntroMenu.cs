61ba95e010acd76cb12e2b04232583a68b1cbea7
introlist <gilflo01@hotmail.com>
2017-05-09 06:47:20 -0500

    v0.92

---
+++ Assets/Scripts/IntroMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroMenu : MonoBehaviour
+{
+    public string sceneName;
+    public Button skip;
+    public Button final;
+
+
+    void Start()
+    {
+        Button btn = skip.GetComponent<Button>();
+        Button btnF = final.GetComponent<Button>();
+        btn.onClick.AddListener(letsGo);
+        btnF.onClick.AddListener(letsGo);
+    }
+	
+	void Update ()
+	{
+
+		
+	}
+
+    void letsGo()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
